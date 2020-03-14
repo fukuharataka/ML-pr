b91aaef40186fe1d5afb5415519451194416feaf
takayukifukuhara <takayuki@curama.jp>
2020-03-14 19:18:25 +0900

    :tada:環境構築とテスト

---
+++ Iris_class/run.py
@@ -0,0 +1,7 @@
+import numpy as np  # 計算用
+import matplotlib.pyplot as plt  # 描画用
+import pandas as pd  # 行列計算、csv読み込み
+# import mglearn
+
+x = np.array([[1, 2, 3], [4, 5, 6]])
+print("x:\n{}".format(x))
---
+++ basicImport.py
@@ -0,0 +1,4 @@
+import numpy as np  # 計算用
+import matplotlib.pyplot as plt  # 描画用
+import pandas as pd  # 行列計算、csv読み込み
+# import mglearn
