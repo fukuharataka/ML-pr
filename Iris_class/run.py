8b713222876127602e4f078d52ea8eb846f86d83
takayukifukuhara <takayuki@curama.jp>
2020-03-14 19:38:46 +0900

    :+1: separate iris data

--- Iris_class/run.py
+++ Iris_class/run.py
@@ -1,12 +1,17 @@
 import numpy as np  # 計算用
 import matplotlib.pyplot as plt  # 描画用
 import pandas as pd  # 行列計算、csv読み込み
-# import mglearn
+import mglearn
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+iris_dataset = load_iris()
 
-x = np.linspace(-10, 10, 100)
+X_train, X_test, y_train, y_test = train_test_split(
+    iris_dataset['data'], iris_dataset['target'], random_state=0
+)
 
-y = np.sin(x)
+print("X_train shape: {}".format(X_train.shape))
+print("y_train_shape: {}".format(y_train.shape))
 
-plt.plot(x, y, marker="x")
-
-plt.show()
+print("X_test_shape: {}".format(X_test.shape))
+print("y_test_shape: {}".format(y_test.shape))
