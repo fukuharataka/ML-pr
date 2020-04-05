91c620213b47d428c514e56914d8ddf3855ab0f1
takayukifukuhara <takayuki@curama.jp>
2020-04-05 17:34:53 +0900

    :+1:K-最近傍法を試した

--- Iris_class/run.py
+++ Iris_class/run.py
@@ -4,33 +4,41 @@ import pandas as pd  # 行列計算、csv読み込み
 import mglearn
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-# from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neighbors import KNeighborsClassifier
 iris_dataset = load_iris()
+knn = KNeighborsClassifier(n_neighbors=1)
 
 X_train, X_test, y_train, y_test = train_test_split(
     iris_dataset['data'], iris_dataset['target'], random_state=0
 )
 
-# print("X_train shape: {}".format(X_train.shape))
-# print("y_train_shape: {}".format(y_train.shape))
-# print("X_test_shape: {}".format(X_test.shape))
-# print("y_test_shape: {}".format(y_test.shape))
-
 iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
 
-pd.plotting.scatter_matrix(
-    iris_dataframe,
-    c=y_train,
-    figsize=(
-        15,
-        15),
-    marker='o',
-    hist_kwds={
-        'bins': 20},
-    s=60,
-    alpha=.8,
-    cmap=mglearn.cm3)
+knn.fit(X_train, y_train)
 
-plt.show()
+X_new = np.array([[5, 2.9, 1, 0.2]])
+prediction = knn.predict(X_new)
 
+print("^^^^^^^^^^^^^^^^")
+print("Prediction: {}".format(prediction))
+print("Predicted target name : {}".format(
+    iris_dataset['target_names'][prediction]))
 
+y_pred = knn.predict(X_test)
+print("Test set predictions: \n {}".format(y_pred))
+print("Test set score: {:.2f}".format(np.mean(y_pred == y_test)))
+
+# pd.plotting.scatter_matrix(
+#     iris_dataframe,
+#     c=y_train,
+#     figsize=(
+#         15,
+#         15),
+#     marker='o',
+#     hist_kwds={
+#         'bins': 20},
+#     s=60,
+#     alpha=.8,
+#     cmap=mglearn.cm3)
+
+# plt.show()
